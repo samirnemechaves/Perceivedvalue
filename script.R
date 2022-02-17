6eb57d19ea5535daee673a6f239262b13d9a0546
Samir Ricardo Neme-Chaves <amigp752@gmail.com>
2022-02-17 14:06:08 -0500

    Updated via the Open Science Framework

--- script.R
+++ script.R
@@ -8,41 +8,39 @@ ipak <- function(pkg){
 }
 
 # usage
-packages <- c("parameters","apa", "dplyr", "skimr","haven","ggplot2","ggpubr","gridExtra","apaTables", "reshape", "GPArotation", "mvtnorm", "psych", "psychometric", "lavaan", "nFactors", "semPlot", "lavaan", "MVN", "semTools", "readr", "readxl")
+packages <- c("car","readxl","parameters","apa", "dplyr", "skimr","haven","ggplot2","ggpubr","gridExtra","apaTables", "reshape", "GPArotation", "mvtnorm", "psych", "psychometric", "lavaan", "nFactors", "semPlot", "lavaan", "MVN", "semTools", "readr", "readxl", "psycho")
 ipak(packages)
-#data
-library(readxl)
-df <- read_excel("valor_percibido_400.xlsx")
-df <- as.data.frame(df)
-skim(df)
+
+Basedatos <- read_excel("BD.xlsx")
+names(Basedatos)
+BD <- Basedatos[1:300,]
+BD <- dplyr::select(BD, VFC1:VC6)
+
+#kMO
+KMO(BD)
+bartlett.test(BD)
+#n factors pakage more info: https://github.com/easystats/parameters/blob/main/R/n_factors.R
+results_nfactor<-n_factors(BD, type = "FA", algorithm = "mle", package = c("nFactors", "psych"))
+plot(results_nfactor)
+results_nfactor
+as.data.frame(results_nfactor)
+#Exploratory Factorial Analysis ASI
+factor<-fa(BD, nfactors = 4,fm = "ml",rotate ="oblimin",cor = "cor")
+print(factor,digits = 2,cut = .30,sort=TRUE)
+
+#Eliminar Problematicos
+names(df)
+df<- dplyr::select(BD, -"VE1", -"VE2", -"VE3", -"VE7", -"VE8", -"VE9")
 #kMO
 KMO(df)
-#n factors ASI
-results_nfactor<-n_factors(df, rotate = "oblimin", fm = "mle", n = NULL)
-plot(results_nfactor)
-results_nfactorASI
-as.data.frame(results_nfactorASI)
-summary(results_nfactorASI)
-#Exploratory Factorial ANalysis ASI
-factor<-fa(df, nfactors = 4,fm = "ml",rotate ="oblimin",cor = "poly")
-print(factor,digits = 2,cut = F,sort=TRUE)
-#eliminar 12, 13, 14
-names(df)
-df2<- dplyr::select(df, -"x12", -"x13", -"x14" )
-#kMO
-KMO(df2)
-bartlett.test(df2)
+bartlett.test(df)
 #cronbach and item-total
-psych::alpha(df2)
+psych::alpha(df)
 #Exploratory Factorial ANalysis ASI
-factor<-fa(df2, nfactors = 4,fm = "ml",rotate ="oblimin",cor = "poly")
-print(factor,digits = 2,cut = F,sort=TRUE)
+results_nfactor2<-n_factors(df, type = "FA", algorithm = "mle", package = c("nFactors", "psych"))
+plot(results_nfactor2)
+factor<-fa(df, nfactors = 4,fm = "ml",rotate ="oblimin",cor = "cor")
+print(factor,digits = 2,cut = .30,sort=TRUE)
 #p-value
 factor$PVAL
 factor$score.cor
-Descriptivos <- read_excel("Descriptivos.xlsx")
-sd(Descriptivos$Edad)
-Genero_Factor <- factor(Descriptivos$`Indique su genero`, levels=c("Femenino", "Masculino"))
-(Genero_Factor)
-Ingresos_Factor <- Factor <- factor(Descriptivos$Ingresos, levels=c("Menos de 700000", "700001 y 1400000", "1400001 y 2100000", "2100000 y 2.800000", "2800001 y 3500000", "3500001, 4200000", "Mas de 4200000" )) 
-(Ingresos_Factor)
